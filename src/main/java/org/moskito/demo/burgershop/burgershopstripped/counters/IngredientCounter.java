49c52e52c4b4a486917183377db5feb80e9e4f98
jpleorx <leo.ertuna@gmail.com>
2017-12-13 18:23:10 +0200

    Counters are now categorized and assigned producer ids

--- src/main/java/org/moskito/demo/burgershop/burgershopstripped/counters/IngredientCounter.java
+++ src/main/java/org/moskito/demo/burgershop/burgershopstripped/counters/IngredientCounter.java
@@ -3,7 +3,8 @@ package org.moskito.demo.burgershop.burgershopstripped.counters;
 import net.anotheria.moskito.aop.annotation.CountByParameter;
 
 public class IngredientCounter {
-    @CountByParameter public void ingredientUsed(String ingredient) {
+    @CountByParameter(category = "business", producerId = "ingredients")
+    public void ingredientUsed(String ingredient) {
 
     }
 }
--- src/main/java/org/moskito/demo/burgershop/burgershopstripped/counters/OrderCounter.java
+++ src/main/java/org/moskito/demo/burgershop/burgershopstripped/counters/OrderCounter.java
@@ -2,7 +2,7 @@ package org.moskito.demo.burgershop.burgershopstripped.counters;
 
 import net.anotheria.moskito.aop.annotation.Count;
 
-@Count
+@Count(category = "business", producerId = "orders")
 public class OrderCounter {
     public void orderPlaced() {
 
